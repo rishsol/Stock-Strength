87e03a095160817f847f5510a762aa93a63ccf0d
rishsol <rishabsolanki09@gmail.com>
2019-07-16 12:36:36 -0400

    Add files via upload

---
+++ AppleWebScrape.py
@@ -0,0 +1,23 @@
+import requests
+import csv
+import time
+from bs4 import BeautifulSoup
+
+
+url = 'https://www.nasdaq.com/symbol/aapl'
+
+page = requests.get(url)
+page.content
+
+soup = BeautifulSoup(page.content, 'html.parser')
+soup.prettify()
+
+price_box = soup.find('div', attrs={'class', 'qwidget-dollar'})
+price = price_box.text
+print('Current Apple Stock Price: ' + price)
+
+
+
+with open('index.csv', 'a') as csv_file:
+  writer = csv.writer(csv_file)
+  writer.writerow([price])
