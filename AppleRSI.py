51156856f6128379b5eff202397c295474c5a456
rishsol <rishabsolanki09@gmail.com>
2019-08-19 17:20:45 -0400

    Visualization for single company RSI calculation complete

--- AppleRSI.py
+++ AppleRSI.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import matplotlib
+import matplotlib.pyplot as plt
 
 url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&apikey=F9HBARA0127RFBID&datatype=csv'
 
@@ -10,7 +10,7 @@ gain_average = []
 loss_average = []
 RSI = pd.Series([])
 
-AAPL_Info = pd.read_csv('/Users/rishabsolanki09@gmail.com/Downloads/daily_AAPL (3).csv')
+AAPL_Info = pd.read_csv('/Users/rishabsolanki09@gmail.com/Downloads/daily_AAPL (5).csv')
 AAPL_Closing = AAPL_Info.loc[:, ['timestamp','close']].iloc[0:28]
 
 change[len(AAPL_Closing) - 1] = 0
@@ -18,17 +18,13 @@ change[len(AAPL_Closing) - 1] = 0
 for i in range(0, len(AAPL_Closing) - 1):
     change[i] = AAPL_Closing['close'][i] - AAPL_Closing['close'][i + 1]
 
-AAPL_Closing.insert(2, 'Change', change)
-
-
-for value in AAPL_Closing['Change']:
+for value in change:
     if(value > 0):
         gain.append(value)
         loss.append(0)
     else:
         loss.append(abs(value))
         gain.append(0)
-
 
 gain_average.append(sum(gain[0:14])/14)
 loss_average.append(sum(loss[0:14])/14)
@@ -39,4 +35,11 @@ for i in range(1, len(gain)):
     loss_average.append((loss_average[i - 1] * 13 + loss[i]) / 14)
     RSI[i] = 100 - (100/(1 + gain_average[i]/loss_average[i]))
 
-print(RSI)
+AAPL_Closing.insert(2, 'RSI', RSI)
+print(AAPL_Closing)
+
+plt.plot(range(28), AAPL_Closing['RSI'].iloc[::-1])
+plt.title('RSI Values of Apple Stock over a 28 Day Period')
+plt.ylabel('RSI')
+plt.xlabel('Day')
+plt.show()
